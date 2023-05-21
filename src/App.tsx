71b2c65fe1aadda41982e9c040194ce4b41af7db
Сергей Викторович Князев <113102209+Knyazew36@users.noreply.github.com>
2023-05-21 17:27:29 +0300

    11:24

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import FavouritesPage from './pages/FavouritesPage';
+import Navigation from './components/Navigation';
+
+const App = () => (
+  <>
+    <Navigation />
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/favourites' element={<FavouritesPage />} />
+    </Routes>
+  </>
+);
+
+export default App;
